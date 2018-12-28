b2e042aad1d90235ddb46e50565f75a8dedaf7d4
Koji Miyazato <viercc@gmail.com>
2018-12-28 16:25:17 +0900

    Turn doctest.sh into run-test.sh which does test-them-all.

--- run-test.sh
+++ run-test.sh
@@ -6,6 +6,8 @@ compilers="ghc-8.2.2 ghc-8.4.3 ghc-8.6.3"
 
 for ghc in $compilers; do
     cabal v2-build -w $ghc --enable-tests .
+    cabal v2-test -w $ghc
+    
     cabal v2-build -w $ghc --enable-tests doctest
     cabal v2-exec -w $ghc -- doctest -isrc src/**/*.hs
 done
