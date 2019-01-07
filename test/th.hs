864fe9dc7d6dc536506135847ac3fc812836a224
Koji Miyazato <viercc@gmail.com>
2019-01-07 17:36:19 +0900

    Add TH generation tests

--- test/th.hs
+++ test/th.hs
@@ -12,6 +12,7 @@ import           Data.Matchable.TH
 main :: IO ()
 main = putStrLn "compiles"
 
+-- Most simple case
 data F a = F0 | F1 a | F2 a a
   deriving (Show, Eq, Functor)
 
@@ -20,6 +21,7 @@ instance Eq1 F where
 
 $(deriveMatchable ''F)
 
+-- Test case for using [] and tuples
 newtype G a = G [(a, Int, a)]
   deriving (Show, Eq, Functor)
 
@@ -28,6 +30,7 @@ instance Eq1 G where
 
 $(deriveMatchable ''G)
 
+-- Test case for extra type variable
 data H a b = H0 a | H1 a b | H2 [Either a b]
   deriving (Show, Eq, Functor)
 
@@ -50,7 +53,8 @@ solver in TH side, so it's not an easy target.
 
 -}
 
-data I a b = I a b (Either [b] (a,b))
+-- Test case for using Matchable and Bimatchable
+data I a b = I a (F b) (Either [b] (a,b))
   deriving (Show, Eq)
 
 instance (Eq a) => Eq1 (I a) where
@@ -61,8 +65,18 @@ instance (Eq a) => Functor (I a) where
 
 $(deriveMatchable ''I)
 
+-- Test case for recursive type
+data J a = J0 | J1 (J a, Int) a (Int, J a)
+  deriving (Show, Eq, Functor)
+
+instance Eq1 J where
+  liftEq = liftEqDefault
+
+$(deriveMatchable ''J)
+
 -------------------------------
 
+-- Most simple case
 data BiF a b = BiF0 | BiF1 a b
 
 instance Eq2 BiF where
@@ -73,6 +87,7 @@ instance Bifunctor BiF where
 
 $(deriveBimatchable ''BiF)
 
+-- Test case for using [], tuple, and another Bimatchable instance
 data BiG a b = BiG0 | BiG1 [a] [b] | BiG2 (Int, BiF a b)
 
 instance Eq2 BiG where
@@ -82,3 +97,14 @@ instance Bifunctor BiG where
   bimap = bimapRecovered
 
 $(deriveBimatchable ''BiG)
+
+-- Test case for recursive type
+data BiH a b = BiH1 a b | BiH2 (BiH b a) (BiH a String)
+
+instance Eq2 BiH where
+  liftEq2 = liftEq2Default
+
+instance Bifunctor BiH where
+  bimap = bimapRecovered
+
+$(deriveBimatchable ''BiH)
