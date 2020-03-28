8ec73e9d592d322682631187bd16ae74bf6e9c08
Koji Miyazato <viercc@gmail.com>
2020-03-28 20:57:01 +0900

    Update test script target (add ghc-8.10 and drop ghc-8.0)
    Dependencies are not available for ghc-8.10 yet,
    but building with --allow-newer passes tests except
    doctest (which is not updated for the new compiler
    at this moment.)

--- run-test.sh
+++ run-test.sh
@@ -2,17 +2,13 @@
 
 set -e
 
-compilers="ghc-8.0 ghc-8.2 ghc-8.4 ghc-8.6 ghc-8.8"
+cabal='cabal'
+compilers="ghc-8.2 ghc-8.4 ghc-8.6 ghc-8.8 ghc-8.10"
 
 for ghc in $compilers; do
-    echo Testing for $($ghc --version)
-    
-    cabal v2-build -w $ghc -v0 --enable-tests matchable matchable-th matchable-examples
-    cabal v2-test -w $ghc -v0 .
-    cabal v2-test -w $ghc -v0 matchable-th/
+    echo Testing for ghc-$($ghc --numeric-version)
 
-    if [ "$ghc" != "ghc-8.0" ]; then
-        cabal v2-build -w $ghc -v0 doctest
-        cabal v2-exec -w $ghc -v0 -- doctest -isrc src/**/*.hs
-    fi
+    $cabal v2-build -w $ghc -v0 -j4 --enable-tests matchable matchable-th matchable-examples
+    $cabal v2-test -w $ghc -v0 .
+    $cabal v2-test -w $ghc -v0 matchable-th/
 done
